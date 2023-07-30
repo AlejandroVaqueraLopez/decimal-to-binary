d1444af3118fd4013bf24f885c0bcc49a8684b51
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2023-07-30 01:35:43 -0700

    feat: finalizado

--- main.cpp
+++ main.cpp
@@ -1,35 +1,45 @@
-﻿#include "pch.h"
+//#include "pch.h"
 #include <iostream>
 #include <string>
 #include <cstring>
-
-using namespace winrt;
-using namespace Windows::Foundation;
+//using namespace winrt;
+//using namespace Windows::Foundation;
 using namespace std;
 
-void Divider(char result[], int divisor, int dividend) {
-    int cocient = dividend / divisor;//base === divisor
-
+void Divider(int result[], int divisor, int dividend, int cont, bool flag) {
+	//Create array of results
+	cont++;
+    int cocient = dividend / divisor;//base == divisor 
     int residual = dividend % divisor;
-    //Method that converts an int to string 
-    string binary = to_string(residual);
-    //Method that converts string to "const char* type"
-    const char* nchar = binary.c_str();
-
-    strcat(result, nchar);
-
-
-    cout << result;
-    //Divider(result, divisor, cocient);
+    
+    if(cocient == 1){
+    	flag = true;
+    	result[cont] = residual;//[0]
+    	Divider(result, divisor, cocient, cont, flag);
+	}else{
+		if(flag){
+			result[cont] = residual;// last 1
+			
+			//rever the array
+			for(int ndCont = cont; ndCont >= 0; ndCont--){
+				cout << result[ndCont];
+			}
+		}else{
+			result[cont] = residual;//[0]
+    		Divider(result, divisor, cocient, cont, flag);	
+		}
+	}
 }
-
 
 int main()
 {
+	int cont = -1;
     int base = 2;
-    int decimalNumber = 5;
-
-    char result[] = "\0";
-    Divider(result, base, decimalNumber);
+    int decimalNumber = 1983;
+    bool flag = false;
+    int result[100];
+    
+    Divider(result, base, decimalNumber, cont, flag);
+    
     return 0;
 }
