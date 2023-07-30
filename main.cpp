7071e8224a91b59d137baf57352673e6e2c95d78
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2023-07-29 21:58:33 -0700

    feat: all

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace std;
+
+void Divider(char result[], int divisor, int dividend) {
+    int cocient = dividend / divisor;//base === divisor
+
+    int residual = dividend % divisor;
+    //Method that converts an int to string 
+    string binary = to_string(residual);
+    //Method that converts string to "const char* type"
+    const char* nchar = binary.c_str();
+
+    strcat(result, nchar);
+
+
+    cout << result;
+    //Divider(result, divisor, cocient);
+}
+
+
+int main()
+{
+    int base = 2;
+    int decimalNumber = 5;
+
+    char result[] = "\0";
+    Divider(result, base, decimalNumber);
+    return 0;
+}
